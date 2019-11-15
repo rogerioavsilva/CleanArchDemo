c6de22b838cb454a8b34e79f31a8f6c8b968319a
Rogério Silva <rogerioavsilva@gmail.com>
2019-11-15 13:06:13 -0300

    Initial Commit of Infrastructure.Data and Domain projects

---
+++ CleanArch/CleanArch.Infra.Data/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infra.Data
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
