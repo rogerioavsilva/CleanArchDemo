b26e3a47c031f91d400c79e171159817bc211799
Rogério Silva <rogerioavsilva@gmail.com>
2019-11-16 00:18:26 -0300

    Implemented IoC layer

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<ICourseService, CourseService>();
+            serviceCollection.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
