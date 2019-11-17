167870b976ac7454994c7bbca5dcd6ad9986947a
Rogério Silva <rogerioavsilva@gmail.com>
2019-11-17 18:59:45 -0300

    Commands, handlers updated course repository cleanup ioc container

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,12 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArch.Infra.IoC
@@ -10,6 +15,8 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddScoped<IMediatorHandler, InMemoryBus>();
+            serviceCollection.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
             serviceCollection.AddScoped<ICourseService, CourseService>();
             serviceCollection.AddScoped<ICourseRepository, CourseRepository>();
         }
